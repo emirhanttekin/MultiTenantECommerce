3addc16555360d028ef8ed3bdc3c871e1a0f2432
Emirhan Tekin <73306905+emirhanttekin@users.noreply.github.com>
2025-02-15 03:28:58 +0300

    category api resources en and  home design

---
+++ MultiTenantECommerce/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuvCeramicArt.Shop.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ MultiTenantECommerce/wwwroot/js/language.js
@@ -0,0 +1,27 @@
+﻿
+    function changeLanguage(lang) {
+        document.cookie = "selectedLanguage=" + lang + "; path=/; expires=" + new Date(new Date().getTime() + 365 * 24 * 60 * 60 * 1000).toUTCString();
+    location.reload(); 
+                        }
+
+    function getLanguageFromCookie() {
+        let cookies = document.cookie.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+        let parts = cookies[i].split('=');
+    if (parts[0] === "selectedLanguage") {
+                                    return parts[1];
+                                }
+                            }
+    return "tr";
+                        }
+
+    function updateLanguageUI() {
+        let lang = getLanguageFromCookie();
+    let flagSrc = lang === "en" ? "/images/flags/eng.png" : "/images/flags/tr.png";
+    let langText = lang === "en" ? "ENG" : "TR";
+
+    document.getElementById("selectedFlag").src = flagSrc;
+    document.getElementById("selectedLangText").innerText = langText;
+                        }
+
+    updateLanguageUI();
